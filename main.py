d905db7e894d36c8119fd81883251946f77d133a
@mikhail_nebasov <nebasov@bk.ru>
2023-09-01 20:18:14 +0300

    model serialized

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from datetime import datetime
+import dill
 import pandas as pd
 from sklearn.preprocessing import TargetEncoder
 from sklearn.model_selection import cross_val_score
 from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.compose import ColumnTransformer
 
 
 # Loading of data without features omitted on EDA stage
@@ -30,16 +39,52 @@ df_sessions.utm_adcontent.fillna('other', inplace=True)
 df_sessions.device_brand.fillna('other', inplace=True)
 
 # Merging of tables and dropping of session_id
-df_full = df_hits_agg.merge(right=df_sessions, on='session_id', how='inner')
+df_full = df_hits_agg.merge(right=df_sessions, on='session_id', how='left')
 df_full = df_full.drop('session_id', axis=1)
 print('Merging of tables performed')
 
-#TENC
-tenc = TargetEncoder(cv=7)
-x_enc = tenc.fit_transform(df_full.drop(columns=['event_action'], axis=1), df_full['event_action'])
+X = df_full.drop(columns=['event_action'], axis=1)
+y = df_full['event_action']
 
-# Model training
-print('Model training...')
-scores = cross_val_score(MLPClassifier(random_state=0, hidden_layer_sizes=(100, 50)), x_enc, df_full['event_action'], cv=5, scoring='roc_auc') #0,6667
-print('Model trained')
-print(scores)
+categorical_transformer = Pipeline(steps=[
+    ('target_encoder', TargetEncoder(cv=7))
+    ])
+
+categorical_features = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_adcontent', 'device_category', 'device_brand',
+    'device_screen_resolution', 'device_browser', 'geo_country', 'geo_city']
+
+to_engineer_features = ColumnTransformer(transformers=[('categorical', categorical_transformer, categorical_features)])
+
+models = [LogisticRegression(random_state=0, solver='liblinear'), RandomForestClassifier(random_state=0, n_jobs=12),
+    MLPClassifier(random_state=0, hidden_layer_sizes=(100, 50))]
+
+preprocessor = Pipeline(steps=[('to_engineer_features', to_engineer_features),])
+
+best_score = 0.0
+best_pipe = None
+for model in models:
+    pipe = Pipeline(steps=[('preprocessor', preprocessor), ('model', model)])
+
+    score = cross_val_score(pipe, X, y, cv=5, scoring='roc_auc')
+    print(f'model: {type(model).__name__}, acc_mean: {score.mean():.4f}, acc_std: {score.std():.4f}')
+
+    if score.mean() > best_score:
+        best_score = score.mean()
+        best_pipe = pipe
+
+best_pipe.fit(X, y)
+print(f'best model: {type(best_pipe.named_steps["model"]).__name__}, accuracy: {best_score:.4f}')
+
+file_name = 'best_pipe.pkl'
+with open(file_name, 'wb') as file:
+    dill.dump({
+        'model':best_pipe,
+        'metadata':{
+            'name':'Target action prediction model',
+            'author':'Mikhail Nebasov',
+            'version':'1.0.0',
+            'date':datetime.now(),
+            'type':type(best_pipe.named_steps['model']).__name__,
+            'accuracy':best_score
+            }
+        }, file)
